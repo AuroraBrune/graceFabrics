8d3650521a1a644f7a687c1d01fbc9b10178a98c
AnthonyStembreit <stembreit@gmail.com>
2020-10-31 12:53:07 -0500

    set up server, MVC, database, and products table.

---
+++ models/products.js
@@ -0,0 +1,29 @@
+module.exports = function(sequelize, DataTypes){
+    let Product = sequelize.define("Product", {
+        type: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        img1:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        img2:{
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        img3:{
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        description:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price:{
+            type: DataTypes.NUMBER,
+            allowNull: false,
+        }
+    })
+    return Product
+}
