7e434e56aa101530890facd1e0ac9e0fd96f4aa4
AnthonyStembreit <stembreit@gmail.com>
2020-11-04 20:53:25 -0600

    adding seeds and changing api-routes to match database inputs

---
+++ gracefabrics/scripts/seeds.js
@@ -0,0 +1,22 @@
+let db = require("../models");
+
+db.Products.create({
+    description: "rainbow stole done in tie-dye",
+    price: 40,
+    img1: "stole picture",
+    type: "Rainbow-Stole",
+})
+
+db.Products.create({
+    description: "easter stole done in tie-dye",
+    price: 40,
+    img1: "stole picture",
+    type: "Easter-Stole",
+})
+
+db.Products.create({
+    description: "christmas stole done in tie-dye",
+    price: 40,
+    img1: "stole picture",
+    type: "Christmas-Stole",
+})
