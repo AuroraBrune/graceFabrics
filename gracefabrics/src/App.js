0ee19565a81d66e710bbc9b129357dacd5215f8c
JordanB <65307981+ubern00bie@users.noreply.github.com>
2020-11-11 21:55:30 -0600

    minor update to app.js

--- gracefabrics/src/App.js
+++ gracefabrics/src/App.js
@@ -11,6 +11,7 @@ import Login from "./pages/Login"
 import Banners from './pages/Banners';
 import Stoles from './pages/Stoles';
 import Admin from './pages/Admin';
+import Orders from './pages/Orders';
 import Contact from './pages/contact';
 
 const classes = makeStyles((theme) => ({
@@ -42,6 +43,7 @@ function App() {
           <Route exact path="/" component={Shop}/>
           <Route path="/contact" component={Contact}/>
           <Route path="/admin" component={Admin}/>
+          <Route path="/orders" component={Orders}/>
         </Grid> 
             {/* 
             <RequireAuth>
