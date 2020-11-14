aa6b7deb59f366c11bfbf0f6d54c5cdd3509ea42
AnthonyStembreit <stembreit@gmail.com>
2020-11-14 12:30:30 -0600

    creating admin management space and new item space

--- gracefabrics/src/pages/ProductManagement.js
+++ gracefabrics/src/pages/ProductManagement.js
@@ -1,38 +1,61 @@
-import React , { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Product from '../components/Product';
 import { Grid } from '@material-ui/core/';
 import API from '../utils/API';
-    
-  const ProductManagement = () => {
+import { Card, Icon, CardTitle, Row, Col } from 'react-materialize'
+const ProductManagement = () => {
     const [productsList, setProducts] = useState({
         products: []
-    }); 
-
+    });
     useEffect(() => {
         API.getProducts().then(results => {
             setProducts({
-                ...productsList, 
+                ...productsList,
                 products: results.data
             });
         });
     }, []);
-
     const productsToMap = productsList.products.map(product => {
+        return (
+            <Row>
+                <Col
+                    m={6}
+                    s={12}
+                >
+                    <Card
+                        closeIcon={<Icon>close</Icon>}
+                        header={<CardTitle image={product.img1} reveal waves="light" />}
+                        reveal={<p>{product.description}</p>}
+                        revealIcon={<Icon>more_vert</Icon>}
+                        title={product.name}
+                    >
+                        <p>
+                            <button>Edit</button>
+                            <button>Delete</button>
+                        </p>
+                    </Card>
+                </Col>
+            </Row>
 
-        return(
-          <div>
-            <li key = {product.id}>
-          
-            </li>
-          </div>
+
         )
     })
-
-    return(
-        <Grid container> 
+    return (
+        <Grid container>
             {productsToMap}
+            <button>Create New Item</button>
         </Grid>
     )
 }
-
 export default ProductManagement;
+
+
+
+
+
+
+
+
+
+
+
