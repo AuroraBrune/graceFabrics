762c07dd8d94ea0a8dd63f8897293feb726d0367
AnthonyStembreit <stembreit@gmail.com>
2021-03-11 17:55:05 -0600

    cleaning up code

--- gracefabrics/src/pages/shop.js
+++ gracefabrics/src/pages/shop.js
@@ -5,24 +5,20 @@ import API from '../utils/API';
 import './shop.css'
 
 const Shop = (props) => {
+
     let [productsList, setProducts] = useState({
         products: []
     });
 
     useEffect(() => {
-        console.log(props)
         API.getSortedStoles(props.type).then(function (res) {
-            console.log("it worked!")
             setProducts({
                 products: res.data
             })
         })
     }, []);
 
-
-
     let productsMaped = productsList.products.map(product => {
-
         return (
             <Grid item xs={9} sm={4} md={3} key={product.id}>
                 <Product
@@ -31,8 +27,6 @@ const Shop = (props) => {
             </Grid>
         )
     })
-
-
 
     return (
         <Grid container>
