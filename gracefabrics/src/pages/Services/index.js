ea6a3d8e6aab5990f59c1783f12c62c43a308d8d
AnthonyStembreit <stembreit@gmail.com>
2021-03-16 16:40:24 -0500

    added class name to service page for consistent styling

--- gracefabrics/src/pages/Services/index.js
+++ gracefabrics/src/pages/Services/index.js
@@ -3,8 +3,8 @@ import { React, useState, useEffect } from 'react';
 export default function Service(props) {
     return (
         <div>
-            <h1>{props.name}</h1>
-            {/* <p>{props.description}</p> */}
+            <h1 className="main">{props.name}</h1>
+            <p className="blurb">Ad sint aute tempor consequat ipsum eiusmod exercitation deserunt mollit. Laboris pariatur esse adipisicing tempor fugiat duis tempor fugiat dolor reprehenderit nulla duis fugiat. Occaecat sit magna exercitation velit aute enim non culpa. Ea proident minim elit laborum nisi cupidatat ea mollit mollit esse elit in nulla. Velit ut cupidatat qui dolor enim ullamco enim occaecat sunt eu consectetur quis et.</p>
         </div>
     )
 }
