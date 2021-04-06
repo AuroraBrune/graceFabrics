f55f77b1ab6da5b0687edcb7a665755c6479d9cd
AnthonyStembreit <stembreit@gmail.com>
2021-04-06 13:15:02 -0500

    added front end code functionality for new password form

--- gracefabrics/src/pages/ResetPassword/index.js
+++ gracefabrics/src/pages/ResetPassword/index.js
@@ -1,27 +1,47 @@
-import { React, useEffect } from 'react';
+import { React, useEffect, useState } from 'react';
 import API from '../../utils/API';
 
 export default function ResetPassword() {
-
+    let [userRecordState, setUserRecordState] = useState({})
     useEffect(() => {
-        let paramArr =  window.location.href.split('?token=')[1].split('&email=');
+        let paramArr = window.location.href.split('?token=')[1].split('&email=');
         let params = {
             token: decodeURIComponent(paramArr[0]),
             email: paramArr[1]
         };
         API.resetPassword(params).then((res) => {
-            if(res.data.showForm === false){
+            if (res.data.showForm === false) {
                 document.getElementById("resetMessage").innerText = res.data.message
             }
-            else{
-                console.log(res.data.record)
-            }
+            console.log(res.data.record)
+            setUserRecordState(res.data.record)
         })
-    } , [])
+    }, [])
+    function passwordCheck(){
+        let newPass = document.getElementById("newPass").value;
+        let verifyPass = document.getElementById("newPassVerify").value;
+        if(newPass !== verifyPass){
+            document.getElementById("resetMessage").innerText = "Passwords do not match! Please try again :)"
+        }
+        let updatePass = {
+            password: newPass,
+            userRecord: userRecordState
+        }
+        // API.updatePasss(updatePass)
+    }
 
     return (
-        <div id="resetMessage">
-            reset password
+        <div>
+            <p id="resetMessage">
+                reset password
+            </p>
+
+            <div>
+                <input id="newPass"label="password" placeholder="New Password"></input>
+                <br></br>
+                <input id="newPassVerify" label="verify password" placeholder="Verify Password"></input>
+                <button onClick={()=>{passwordCheck}}>submit</button>
+            </div>
         </div>
     )
 }
